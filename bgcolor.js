e5adc2b894cf4050cb4425e76b217952052069e5
vskrishna0522 <120077783+vskrishna0522@users.noreply.github.com>
2023-01-28 15:23:18 +0530

    Add files via upload

---
+++ bgcolor.js
@@ -0,0 +1,19 @@
+function changeColor_MOver() {
+    document.getElementById('mover').style.backgroundColor = "yellow"
+}
+function chageColor_MOut() {
+    document.getElementById('mout').style.backgroundColor = "green"
+}
+function changeColor_Click() {
+    document.getElementById('mclick').style.backgroundColor = "red"
+}
+function changeColor_DblClick() {
+    document.getElementById('mdblclick').style.backgroundColor = "blue"
+}
+function changeColor_onfocus(){
+    document.getElementById('monfocus').style.backgroundColor= "red"    
+}
+function myFunction() {
+    let x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+  }
