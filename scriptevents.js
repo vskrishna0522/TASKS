fcc0cdd5b69fb1fb0838f87ddac73f65a5e739d4
vskrishna0522 <120077783+vskrishna0522@users.noreply.github.com>
2023-03-11 08:20:41 +0530

    Add files via upload

---
+++ scriptevents.js
@@ -0,0 +1,17 @@
+/*function dog(brownie){
+    return "kittu"+ brownie
+}
+let result = dog("of kittu's home ")
+console.log(result)*/
+
+/*function krishna (nandhi){
+    return "lord"+ nandhi
+}
+let result = krishna  ("god")
+console.log(result)*/
+
+let krishna = (nandhi) =>{
+    return "lord" +(nandhi)
+}
+let gin = krishna("father")
+console.log(gin)
