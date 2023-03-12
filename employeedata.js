196e108046352383ebbe6bac87eb6d186b628565
vskrishna0522 <120077783+vskrishna0522@users.noreply.github.com>
2023-03-12 21:53:09 +0530

    Add files via upload

---
+++ employeedata.js
@@ -0,0 +1,7 @@
+let employee = [{name:"rahul", email:"kittu@gmail.com", sal:987}, 
+                {name:"hul", email:"hul@gmail.com", sal:99887}, 
+                {name:"ookkul", email:"ppjkiu@gmail.com", sal:1985187}]
+
+ function displaydata(){
+    console.log('hiiiii')
+ }               
